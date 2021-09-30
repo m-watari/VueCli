d17c68877d84b1f565af9a1d19ed2232e4a80a6f
mamoru-watari <watari141@gmail.com>
2021-09-30 23:54:05 +0900

    subページ作成、json読み込み

---
+++ server/src/components/Sub.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul v-for="item in items" :key="item.title">
+      <li>
+        {{ item.title }} : {{ item.description }}
+      </li>
+    </ul>
+    <div>{{ postsData }}</div>
+  </div>
+</template>
+
+<script>
+import testJson from '../assets/test'
+export default {
+  name: 'Sub',
+  data () {
+    return {
+      msg: 'Welcome to SubPage',
+      items: testJson.sampleData,
+      postsData: '',
+      fetchUrl: process.env.FETCH_URL
+    }
+  },
+  // created () {
+  // },
+  // compute: {
+  // },
+  created: async function () {
+    const response = await fetch(this.fetchUrl)
+    this.postsData = await response.json()
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- server/src/router/index.js
+++ server/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Sub from '@/components/Sub'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/sub',
+      name: 'Sub',
+      component: Sub
     }
   ]
 })
